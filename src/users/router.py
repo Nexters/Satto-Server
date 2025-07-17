c2563835c80dceebe7b13c368fd58e4269a5d028
Gukhee Jo <nb92092@navercorp.com>
2025-07-17 23:00:47 +0900

    fix: .idea 제거

--- src/users/router.py
+++ src/users/router.py
@@ -8,8 +8,4 @@ user_router = APIRouter(prefix="/users", tags=["user"])
 
 @user_router.get("")
 async def get_user(request: Request, user_service: UserService = Depends()):
-    """
-    현재 로그인한 사용자의 정보를 반환합니다.
-    """
-
     return await user_service.test()
