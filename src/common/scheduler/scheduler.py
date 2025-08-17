6a1ea52220ffa166521271b6711e150798bdf367
Gukhee Jo <nb92092@navercorp.com>
2025-08-17 22:17:35 +0900

    fix: 스케줄러 요일 추가

--- src/common/scheduler/scheduler.py
+++ src/common/scheduler/scheduler.py
@@ -4,4 +4,4 @@ from src.common.scheduler.tasks import update_next_lotto_draw
 
 scheduler = AsyncIOScheduler(timezone="Asia/Seoul")
 
-scheduler.add_job(update_next_lotto_draw, "cron", hour="21", minute="00")
+scheduler.add_job(update_next_lotto_draw, "cron", day_of_week="sat", hour="21", minute="0")
